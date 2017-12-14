83a5478f101889acd74392e584fd376dc42d63dd
Isashi <isashimitsui@gmail.com>
2017-12-14 12:14:41 +0100

    small fix for avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,21 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, size: 160)
+  def gravatar_for_index(user, size: 160)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
     image_tag(gravatar_url, alt: user.username, class: "card-img-top")
   end
 
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+  end
+
   def gravatar_for_header(user, size: 20)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}?d=mm"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "gravatar_header")
   end
 end
