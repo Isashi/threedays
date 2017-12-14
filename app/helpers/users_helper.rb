c19597ead99f5454d436c7e185f8bb970ae1d9f0
Isashi <isashimitsui@gmail.com>
2017-12-14 17:56:52 +0100

    add comments layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,15 +7,21 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.username, class: "card-img-top")
   end
 
+  def gravatar_for_comments(user, size: 160)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
+    image_tag(gravatar_url, alt: user.username, class: "img-fluid user-photo")
+  end  
+
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
     image_tag(gravatar_url, alt: user.username, class: "gravatar")
   end
 
   def gravatar_for_header(user, size: 20)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
     image_tag(gravatar_url, alt: user.username, class: "gravatar_header")
   end
 end
